8cbfa271d0711e653c8a1166a0163c047baacf0d
FeliiipeAC <lfarayac@gmail.com>
2026-06-10 20:29:49 -0400

    feat: agrega ProductListComponent con map y key

---
+++ src/components/ProductListComponent/ProductListComponent.jsx
@@ -0,0 +1,22 @@
+import CardComponent from '../CardComponent'
+import './ProductListComponent.css'
+
+function ProductListComponent({ products, onAdd}) {
+    if (!products || products.length ===0) {
+        return (
+            <p clasName="product-list__empty">
+                No se encontraron productos para tu busqueda. 🔍
+            </p>
+        )
+    }
+
+    return (
+        <section className="product-list">
+            {products.map ((producto) => (
+                <CardComponent key={producto.id} product={producto} on Add={onAdd} />
+            ))}
+        </section>
+    )
+}
+
+export default ProductListComponent
